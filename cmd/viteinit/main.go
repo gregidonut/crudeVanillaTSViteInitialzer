7bb84c628716396cbca1ded36704692d284a6409
Gregory Babajide Villaflor Osilaja <gregosilaja@outlook.com>
2024-03-28 08:13:47 +0800

    feat: remove the project name logic
    Apparently with npx's create-vite@latest command specifying the dir to
    be `.` can just take the current working dir as the project name so it
    is unnecessary for me to make an implementation for it in the app

--- cmd/viteinit/main.go
+++ cmd/viteinit/main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-	"path/filepath"
-)
-
 func main() {
-	currentDir, err := os.Getwd()
-	if err != nil {
-		log.Printf("Error: %v\n", err)
-		return
-	}
 
-	projectName := filepath.Base(currentDir)
-	fmt.Println("project name:", projectName)
 }
