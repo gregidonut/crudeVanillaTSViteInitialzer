848bf24142d2ab751d2a98f0e8db570951a98030
Gregory Babajide Villaflor Osilaja <gregosilaja@outlook.com>
2024-03-28 18:48:28 +0800

    feat: add rest of the steps to complete the initializer
    - sed commands for index.html
    - sed command for package.json
    - git commands

--- cmd/viteinit/main.go
+++ cmd/viteinit/main.go
@@ -33,7 +33,7 @@ func main() {
 
 	fmt.Println("Project name: ", manifest.projectName)
 
-	commands := []runcommand.Command{
+	initialCommands := []runcommand.Command{
 		{
 			Comment: "create vite app...",
 			Cmd:     "npm",
@@ -75,11 +75,105 @@ func main() {
 			},
 		},
 	}
-	for _, command := range commands {
+	for _, command := range initialCommands {
 		if err := command.RunCmd(); err != nil {
 			log.Fatal(err)
 		}
 	}
+
+	wg := sync.WaitGroup{}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		sedCommandsForIndexHtml := []runcommand.Command{
+			{
+				Comment: "replacing title tag inner text in index.html...",
+				Cmd:     "sed",
+				Args: []string{
+					"-i",
+					fmt.Sprintf("s|<title>[^<]*<\\/title>|<title>%s<\\/title>|g", manifest.projectName),
+					"src/index.html",
+				},
+			},
+			{
+				Comment: "replacing h1 tag inner text in index.html...",
+				Cmd:     "sed",
+				Args: []string{
+					"-i",
+					fmt.Sprintf("s|<h1>[^<]*<\\/h1>|<h1>%s<\\/h1>|g", manifest.projectName),
+					"src/index.html",
+				},
+			},
+		}
+
+		for _, command := range sedCommandsForIndexHtml {
+			if err := command.RunCmd(); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		sedCommandsForPackageJson := runcommand.Command{
+			Comment: "replacing build script, and appending lint script in package.json...",
+			Cmd:     "sed",
+			Args: []string{
+				"-i",
+				`s|"build": "tsc \&\& vite build"|"build": "eslint \&\& tsc \&\& vite build",\n    "lint": "eslint . --ext .ts"|`,
+				"package.json",
+			},
+		}
+		if err := sedCommandsForPackageJson.RunCmd(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	wg.Wait()
+
+	gitCommands := []runcommand.Command{
+		{
+			Comment: "git init",
+			Cmd:     "git",
+			Args: []string{
+				"init",
+			},
+		},
+		{
+			Comment: "git add -A",
+			Cmd:     "git",
+			Args: []string{
+				"add",
+				"-A",
+			},
+		},
+		{
+			Comment: "git commit",
+			Cmd:     "git",
+			Args: []string{
+				"commit",
+				"-m",
+				"Initialize project",
+				"-m",
+				`- vanilla-ts vite, with prettier and eslint
+- added node.gitignore github template to vite generated gitignore
+- implemented a crude directory walk in the vite.config.ts file so that
+  I can mimic regular static file server
+- got to hello world equivalent index.html`,
+			},
+		},
+	}
+
+	for _, command := range gitCommands {
+		if err := command.RunCmd(); err != nil {
+			log.Fatal(err)
+		}
+
+	}
+
 }
 
 type projectManifest struct {
