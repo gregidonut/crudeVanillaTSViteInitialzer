33f3f849cbee2b47e13a367cfb37e3f5268d9bc9
Gregory Babajide Villaflor Osilaja <gregosilaja@outlook.com>
2024-03-28 08:44:56 +0800

    feat: add first command
    More of a proof of concept and sanity check to make sure the thing is
    working, I should now just be able to run some commands in here to
    make sure everything is done correctly in sequence

--- cmd/viteinit/main.go
+++ cmd/viteinit/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+)
 
+const (
+	NPX = "npx"
+)
+
+func main() {
+	runCmd(
+		"create vite app...",
+		NPX,
+		[]string{"create-vite@latest", ".", "--template", "vanilla-ts", "--force"},
+	)
+}
+
+func runCmd(comment, command string, args []string) {
+	fmt.Println(comment)
+	cmd := exec.Command(command, args...)
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = io.MultiWriter(os.Stdout, &stdout)
+	cmd.Stderr = io.MultiWriter(os.Stderr, &stderr)
+
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+	}
 }
