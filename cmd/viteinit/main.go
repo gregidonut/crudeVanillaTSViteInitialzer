867fdeaec18dff0a5115170e72b7668de4443dda
Gregory Babajide Villaflor Osilaja <gregosilaja@outlook.com>
2024-03-28 15:06:58 +0800

    feat: add copy command from a reference dir
    I'm including the reference dir in the repo so that i can get it or fix
    it if something is wrong. this should be in a more suitable place in the
    os though and passed as an environment variable path
    
    - included logic for cp command
    - added definition of environment variable name
    - included check to make sure the dir taken from the environment
      variable exists
    - ended up moving the checks into it's own helper function that i had
      fun writing concurrency for so that i can see all errors that might
      come in one go instead of shutting the app down when one of them fails
      at a time

--- cmd/viteinit/main.go
+++ cmd/viteinit/main.go
@@ -1,27 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gregidonut/crudeVanillaTSViteInitialzer/cmd/viteinit/runcommand"
 	"log"
 	"os"
 	"path/filepath"
+	"sync"
 	"unicode"
 )
 
-func main() {
-	currentDir, err := os.Getwd()
-	if err != nil {
-		log.Fatalln("Error:", err)
-		return
-	}
+const VITEINIT_REFERENCE_PATH = "VITEINIT_REFERENCE_PATH"
 
-	projectName := filepath.Base(currentDir)
-	// vite prompts for name if project name is not lowercase
-	// and I want to avoid dealing with prompts
-	for _, char := range projectName {
-		if unicode.IsUpper(char) {
-			log.Fatalf("Error: project name: '%s' cannot have upper case letters\n", projectName)
+func main() {
+	fmt.Println("---------------------")
+	fmt.Println()
+	referenceAppPath, errors := errorCheck()
+	if len(errors) > 0 || errors == nil {
+		for _, err := range errors {
+			if _, err = fmt.Fprintf(os.Stderr, "Error: %v\n", err); err != nil {
+				log.Fatal(err)
+			}
 		}
+		os.Exit(1)
 	}
 
 	commands := []runcommand.Command{
@@ -50,10 +51,125 @@ func main() {
 			Cmd:     "rm",
 			Args:    []string{"-r", "public", "src", ".gitignore", "index.html"},
 		},
+		{
+			Comment: "copying src, .eslintrc, .eslintignore, .prettierrc, .prettierignore, vite.config and .gitignore",
+			Cmd:     "cp",
+			Args: []string{
+				"-r",
+				filepath.Join(referenceAppPath, "src"),
+				filepath.Join(referenceAppPath, ".eslintrc.cjs"),
+				filepath.Join(referenceAppPath, ".eslintignore"),
+				filepath.Join(referenceAppPath, ".prettierrc.cjs"),
+				filepath.Join(referenceAppPath, ".prettierignore"),
+				filepath.Join(referenceAppPath, "vite.config.ts"),
+				filepath.Join(referenceAppPath, ".gitignore"),
+				".",
+			},
+		},
 	}
 	for _, command := range commands {
 		if err := command.RunCmd(); err != nil {
 			log.Fatal(err)
 		}
 	}
+}
+
+func errorCheck() (string, []error) {
+	wg := sync.WaitGroup{}
+
+	type result struct {
+		referenceAppPath string
+		err              error
+	}
+
+	resultChan := make(chan result)
+	errors := []error{}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		currentDir, err := os.Getwd()
+		if err != nil {
+			resultChan <- result{
+				err: fmt.Errorf("error: %v", err),
+			}
+			return
+		}
+
+		projectName := filepath.Base(currentDir)
+		// vite prompts for name if project name is not lowercase
+		// and I want to avoid dealing with prompts
+		for _, char := range projectName {
+			if unicode.IsUpper(char) {
+				resultChan <- result{
+					err: fmt.Errorf(
+						"error: project name: '%s' cannot have upper case letters", projectName,
+					),
+				}
+				return
+			}
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		referenceAppPath := os.Getenv(VITEINIT_REFERENCE_PATH)
+		if referenceAppPath == "" {
+			resultChan <- result{
+				referenceAppPath: "",
+				err: fmt.Errorf(
+					"could not get reference path at '%s' env var", VITEINIT_REFERENCE_PATH,
+				),
+			}
+			return
+		}
+		_, err := os.Stat(referenceAppPath)
+		if err != nil {
+			if os.IsNotExist(err) {
+				resultChan <- result{
+					referenceAppPath: "",
+					err: fmt.Errorf(
+						"directory from env var '%s' does not exist", referenceAppPath,
+					),
+				}
+				return
+			}
+			resultChan <- result{
+				referenceAppPath: "",
+				err:              fmt.Errorf("error: %v", err),
+			}
+			return
+		}
+
+		resultChan <- result{
+			referenceAppPath: referenceAppPath,
+		}
+	}()
+
+	go func() {
+		wg.Wait()
+		close(resultChan)
+	}()
+
+	referenceAppPath := ""
+
+outer:
+	for {
+		select {
+		case r, ok := <-resultChan:
+			if !ok {
+				break outer
+			}
+			if r.err != nil {
+				errors = append(errors, r.err)
+				continue
+			}
+			referenceAppPath = r.referenceAppPath
+		}
+	}
+
+	return referenceAppPath, errors
 }
